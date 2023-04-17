62726a1982edf87d906c2154306fc5e4f0ff979d
Alex A <alex.andsten@gmail.com>
2023-04-17 14:47:10 +0200

    cart-fix-basics

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,19 +4,19 @@ import React from "react";
 
 const Cart = ({cartItems}) => {
   
-  
+  console.log(cartItems.cartItems)
   return (
     <>
       <h2>Cart</h2>
       <ul>
 
-        {cartItems.cartItems.length ?
+        {
         cartItems.cartItems.map((car) => (
           <li key={car._id}>
             {car.title} - {car.price} 
           </li>
         ))
-        : <p>Error</p>
+       
       }
       </ul>
     </>
