5baa509e963c5fc55642e63aeb42746c61a577bf
Freddan <f.eriksson86@gmail.com>
2023-04-17 19:15:08 +0200

    Halvklar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Nav from './Nav'
-import Cart from './Cart'
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Cart from './Cart';
 
-const Header = (cartItems) => {
+const Header = ({ cartItems, onCartClick }) => {
+  const [showCart, setShowCart] = useState(false);
+  
+  const handleCartClick = () => {
+    setShowCart(!showCart);
+  };
+
   return (
     <>
-    
-     <Nav />
-     <Cart cartItems={cartItems}/>
-
+      <Nav onCartClick={handleCartClick} />
+      {showCart && <Cart cartItems={cartItems} />}
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
