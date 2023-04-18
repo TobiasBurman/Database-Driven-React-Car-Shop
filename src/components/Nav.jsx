492ab7f2c0ac311acb7118f50ac7b286d108ba26
Alex A <alex.andsten@gmail.com>
2023-04-18 14:09:34 +0200

    cart-basics

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styles from './Nav.module.css';
+import { useOutletContext } from "react-router-dom";
+
+const Nav = ({ onCartClick }) => {
 
 
-const Nav = ({ onCartClick, cartItems }) => {
-  const cartItemCount = cartItems.length;
+  const cartItemCount = 0;
+
 
 
   return (
     <div className={styles.nav}>
       <Link to="pages/admin/ManageProducts">Admin</Link>
       <br />
-      <Link to="/">Home</Link>
+      <Link to="/pages/Products">Home</Link>
       <br />
       <div onClick={onCartClick}>Cart({cartItemCount})</div>
     </div>
