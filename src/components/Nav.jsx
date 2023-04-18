9703e0c80be6b5dbfadfd3cbd0803627d5d28474
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-04-18 11:02:14 +0200

    done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import styles from './Nav.module.css'
+import styles from './Nav.module.css';
 
-const Nav = ({onCartClick}) => {
+
+const Nav = ({ onCartClick, cartItems }) => {
+  const cartItemCount = cartItems.length;
+
+
   return (
     <div className={styles.nav}>
-           <Link to="pages/admin/ManageProducts">Admin</Link>
-           <br />
-           <Link to="/">Home</Link>
-           <br />
-           <div onClick={onCartClick}>Cart</div>
+      <Link to="pages/admin/ManageProducts">Admin</Link>
+      <br />
+      <Link to="/">Home</Link>
+      <br />
+      <div onClick={onCartClick}>Cart({cartItemCount})</div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
