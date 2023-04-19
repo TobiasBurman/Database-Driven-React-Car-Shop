201abeeb83629faf889068b67183ea2459fea441
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-04-19 13:46:36 +0200

    halvklar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from './Cart.module.css'
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion"
+
 
 
 const Cart = ({ cartItems, handleRemoveItem, handleRemoveAll }) => {
